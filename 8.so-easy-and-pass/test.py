bd1f409ca4579e8e8b608a13b581d869c422ec4d
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-21 17:03:57 +0800

    chore: test 9

---
+++ 8.so-easy-and-pass/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    @unittest.skip('so easy & pass')
+    def test_siri(self):
+      return
+
+    @unittest.skip('so easy & pass')
+    def test_wxp(self):
+      return
+
+if __name__ == '__main__':
+    unittest.main()
--- 9.palindrome-number/test.py
+++ 9.palindrome-number/test.py
@@ -1,15 +1,12 @@
 import unittest
 from siri import Solution
-from wxp import sum_list
 
 class TestStringMethods(unittest.TestCase):
 
     def test_siri(self):
       s = Solution()
-      self.assertEqual(s.twoSum([1, 2, 3, 4], 4), [0, 2])
-
-    def test_wxp(self):
-      self.assertEqual(sum_list([1, 2, 3, 4], 4), [0, 2])
+      self.assertTrue(s.isPalindrome(123454321))
+      self.assertFalse(s.isPalindrome(-121))
 
 if __name__ == '__main__':
     unittest.main()
