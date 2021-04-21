e3a3a680f2b6a3f24fc9f468e92141d5da4915ab
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-21 16:51:58 +0800

    chore: test 4

--- 12.integer-to-roman/test.py
+++ 12.integer-to-roman/test.py
@@ -1,15 +1,15 @@
 import unittest
 from siri import Solution
-from wxp import maddle_number
+from wxp import sum_list
 
 class TestStringMethods(unittest.TestCase):
 
     def test_siri(self):
       s = Solution()
-      self.assertEqual(s.findMedianSortedArrays([1, 2], [3 ,4]), 2.5)
+      self.assertEqual(s.twoSum([1, 2, 3, 4], 4), [0, 2])
 
     def test_wxp(self):
-      self.assertEqual(maddle_number([1, 2], [3 ,4]), 2.5)
+      self.assertEqual(sum_list([1, 2, 3, 4], 4), [0, 2])
 
 if __name__ == '__main__':
     unittest.main()
