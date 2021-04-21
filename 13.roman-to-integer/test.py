0211c4444dbf493d5130bc034e0b6d9414870300
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-21 17:09:02 +0800

    chore: test 13

--- 13.roman-to-integer/test.py
+++ 13.roman-to-integer/test.py
@@ -1,15 +1,16 @@
 import unittest
 from siri import Solution
-from wxp import sum_list
 
 class TestStringMethods(unittest.TestCase):
 
     def test_siri(self):
       s = Solution()
-      self.assertEqual(s.twoSum([1, 2, 3, 4], 4), [0, 2])
+      self.assertEqual(s.romanToInt('MCMXCIV'), 1994)
+      self.assertEqual(s.romanToInt('LVIII'), 58)
+
 
     def test_wxp(self):
-      self.assertEqual(sum_list([1, 2, 3, 4], 4), [0, 2])
+      return
 
 if __name__ == '__main__':
     unittest.main()
