5d7939330274eacde7948ab434ddccb89ef4a86b
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-21 17:05:31 +0800

    chore: test 10

--- 10.regular-expression-matching/test.py
+++ 10.regular-expression-matching/test.py
@@ -1,15 +1,13 @@
 import unittest
 from siri import Solution
-from wxp import sum_list
 
 class TestStringMethods(unittest.TestCase):
 
     def test_siri(self):
       s = Solution()
-      self.assertEqual(s.twoSum([1, 2, 3, 4], 4), [0, 2])
-
-    def test_wxp(self):
-      self.assertEqual(sum_list([1, 2, 3, 4], 4), [0, 2])
+      self.assertTrue(s.isMatch('aab', 'c*a*b'))
+      self.assertFalse(s.isMatch('mississippi', 'mis*is*p*.'))
+      self.assertTrue(s.isMatch('ahginragranoginaogi', '.*'))
 
 if __name__ == '__main__':
     unittest.main()
