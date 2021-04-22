44e87b1896768c25012a4ccbedba885d8e892be5
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-22 19:02:50 +0800

    siri number 16

---
+++ problems/16-3sum-closest/siri.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int):
+      result = nums[0] + nums[1] + nums[2]
+      has_find_dict = dict()
+
+      for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+          if nums[j] in has_find_dict:
+            continue
+          for z in range(j + 1, len(nums)):
+            if nums[z] in has_find_dict:
+              continue
+            if abs(nums[i] + nums[j] + nums[z] - target) < abs(result):
+              result = nums[i] + nums[j] + nums[z]
+
+      has_find_dict[i] = True
+
+      return result
