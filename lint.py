7cf49c7633501ca46e8f00621440758767bc8b01
liuxinrui.wxp <liuxinrui.wxp@bytedance.com>
2021-04-25 21:10:27 +0800

    fix: remove lint.py

--- lint.py
+++
@@ -1,8 +0,0 @@
-# from pylint import epylint as lint
-# (pylint_stdout, pylint_stderr) = lint.py_run('./problems/1-two_nums/siri.py --disable C0114', return_std=True)
-
-# print(pylint_stdout, pylint_stderr)
-
-import pylint.lint
-pylint_opts = ['-h']
-pylint.lint.Run(pylint_opts)
